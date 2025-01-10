aa78e4fe3f8e952f35f47621efe09c86551209fc
kgriffo <karsten.griffo@gmail.com>
2025-01-10 13:04:58 -0500

    added client ID to visible file for pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import Auth from "./App.jsx";
 
-const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+const clientId =
+  "248005865613-j5nc0j164et50emv7g18ap21l64bi07q.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={clientId}>
