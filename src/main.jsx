4b3f79ef889cbc757f237e9d7755c1ab2b36529d
kgriffo <karsten.griffo@gmail.com>
2025-01-10 10:48:21 -0500

    preparing to switch repo to public - hiding google client ID

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 import Auth from "./App.jsx";
 
-const clientId =
-  "248005865613-9qvvrcc519rvdfp444dfio0mvq14q43m.apps.googleusercontent.com"; // Replace with your Google OAuth Client ID
+const clientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={clientId}>
