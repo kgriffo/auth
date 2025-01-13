3589d05c343b628f34c26cb4c7c6f593c2d1a2f1
kgriffo <karsten.griffo@gmail.com>
2025-01-13 11:33:17 -0500

    Deleted some commented code, trying some stuff out

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-/* import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import { GoogleLogin } from "@react-oauth/google"; */
 import { useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
 import "./App.css";
@@ -10,8 +6,6 @@ function Auth() {
   const googleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
       if (tokenResponse.authuser == "0") {
-        console.log("Not an authorized user; login failed.");
-        alert("Not an authorized user; login failed.");
         return;
       }
       console.log(tokenResponse);
@@ -33,36 +27,4 @@ function Auth() {
   );
 }
 
-/* function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>This is a test string</p>
-      <p>Hot-reloading is cool! Yes it is</p>
-    </>
-  );
-} */
-
-//export default App;
 export default Auth;
