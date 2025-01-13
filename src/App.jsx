e63b2d4bd909a91a6f549774771b78bd5d84dc30
kgriffo <karsten.griffo@gmail.com>
2025-01-13 11:46:53 -0500

    Still trying to fix small UI issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,19 @@ import "./App.css";
 function Auth() {
   const googleLogin = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
-      if (tokenResponse.authuser == "0") {
-        return;
-      }
-      if (tokenResponse.authuser == "0") {
+      if (tokenResponse.authuser == "1") {
+        console.log(tokenResponse);
+        const userInfo = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
+        );
+
+        console.log(userInfo);
+        alert("Login successful!");
+      } else {
         console.log("Unauthorized user; login failed.");
         alert("Unauthorized user; login failed.");
       }
-      console.log(tokenResponse);
-      const userInfo = await axios.get(
-        "https://www.googleapis.com/oauth2/v3/userinfo",
-        { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
-      );
-
-      console.log(userInfo);
-      alert("Login successful!");
     },
     onError: (errorResponse) => console.log(errorResponse),
   });
