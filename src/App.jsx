e74c193117572724414c77234724d22d74ff4357
kgriffo <karsten.griffo@gmail.com>
2025-01-15 13:48:09 -0500

    Testing some stuff with scope

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function Auth() {
       );
       xhr.send();
     },
-    //scope: "openid email profile", //basic scope included since scope is required here
+    scope: "https://www.googleapis.com/auth/calendar.readonly",
   });
 
   // const googleLogin = useGoogleLogin({
