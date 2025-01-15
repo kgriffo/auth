f9e5985506698e02c97e3c312f301f21de928cd0
kgriffo <karsten.griffo@gmail.com>
2025-01-15 12:43:27 -0500

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import axios from "axios";
-import { google } from "@react-oauth/google";
+import { google } from "google";
 import { useGoogleLogin } from "@react-oauth/google";
 
 function Auth() {
@@ -11,28 +11,28 @@ function Auth() {
     scope: "openid email profile", //basic scope included since scope is required here
   });
 
-  client.requestAccessToken();
-  const googleLogin = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      if (tokenResponse.authuser == "1") {
-        console.log(tokenResponse);
-        const userInfo = await axios.get(
-          "https://www.googleapis.com/oauth2/v3/userinfo",
-          { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
-        );
+  // client.requestAccessToken();
+  // const googleLogin = useGoogleLogin({
+  //   onSuccess: async (tokenResponse) => {
+  //     if (tokenResponse.authuser == "1") {
+  //       console.log(tokenResponse);
+  //       const userInfo = await axios.get(
+  //         "https://www.googleapis.com/oauth2/v3/userinfo",
+  //         { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
+  //       );
 
-        console.log(userInfo);
-        alert("Login successful!");
-      } else {
-        console.log("Unauthorized user; login failed.");
-        // Timeout needed to ensure Google login pop-up closes before alert appears
-        setTimeout(() => {
-          alert("Unauthorized user; login failed.");
-        }, 100);
-      }
-    },
-    onError: (errorResponse) => console.log(errorResponse),
-  });
+  //       console.log(userInfo);
+  //       alert("Login successful!");
+  //     } else {
+  //       console.log("Unauthorized user; login failed.");
+  //       // Timeout needed to ensure Google login pop-up closes before alert appears
+  //       setTimeout(() => {
+  //         alert("Unauthorized user; login failed.");
+  //       }, 100);
+  //     }
+  //   },
+  //   onError: (errorResponse) => console.log(errorResponse),
+  // });
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Google OAuth Test</h1>
