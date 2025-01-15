b5fb4fd10eb48bbad8f2029761f6ed085a04e53d
kgriffo <karsten.griffo@gmail.com>
2025-01-15 11:23:08 -0500

    Added Google client library

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Auth from "./App.jsx";
 
 const clientId =
   "248005865613-j5nc0j164et50emv7g18ap21l64bi07q.apps.googleusercontent.com";
+<script src="https://accounts.google.com/gsi/client" async></script>;
 
 createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={clientId}>
