6c4240b42b4d112594fd618293acaac7dec12f47
kgriffo <karsten.griffo@gmail.com>
2025-01-22 13:10:41 -0500

    testing out some react router stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,16 @@
 import "./App.css";
+//import React, { useContext, createContext, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  PrivateRoute,
+  Link,
+  //Redirect,
+  //useHistory,
+  //useLocation,
+} from "react-router-dom";
+
 //import axios from "axios";
 //import { useGoogleLogin } from "@react-oauth/google";
 
@@ -13,35 +25,63 @@ function Auth() {
     },
   });
 
-  // const googleLogin = useGoogleLogin({
-  //   onSuccess: async (tokenResponse) => {
-  //     if (tokenResponse.authuser == "1") {
-  //       console.log(tokenResponse);
-  //       const userInfo = await axios.get(
-  //         "https://www.googleapis.com/oauth2/v3/userinfo",
-  //         { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
-  //       );
-
-  //       console.log(userInfo);
-  //       alert("Login successful!");
-  //     } else {
-  //       console.log("Unauthorized user; login failed.");
-  //       // Timeout needed to ensure Google login pop-up closes before alert appears
-  //       setTimeout(() => {
-  //         alert("Unauthorized user; login failed.");
-  //       }, 100);
-  //     }
-  //   },
-  //   onError: (errorResponse) => console.log(errorResponse),
-  // });
-
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Google OAuth Test</h1>
-      <button onClick={() => client.requestAccessToken()}>Authorize me</button>
-      {/* <button onClick={() => googleLogin()}>Login with Google</button> */}
-    </div>
+    <>
+      {/* <ProvideAuth> */}
+      <Router>
+        <div>
+          {/* <AuthButton /> */}
+
+          <ul>
+            <li>
+              <Link to="/public">Public Page</Link>
+            </li>
+            <li>
+              <Link to="/protected">Protected Page</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/public">{/* <PublicPage /> */}</Route>
+            <Route path="/login">{/* <LoginPage /> */}</Route>
+            <PrivateRoute path="/protected">
+              {/* <ProtectedPage /> */}
+            </PrivateRoute>
+          </Switch>
+        </div>
+      </Router>
+      {/* </ProvideAuth> */}
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>Google OAuth Test</h1>
+        <button onClick={() => client.requestAccessToken()}>
+          Authorize me
+        </button>
+        {/* <button onClick={() => googleLogin()}>Login with Google</button> */}
+      </div>
+    </>
   );
 }
 
 export default Auth;
+
+// const googleLogin = useGoogleLogin({
+//   onSuccess: async (tokenResponse) => {
+//     if (tokenResponse.authuser == "1") {
+//       console.log(tokenResponse);
+//       const userInfo = await axios.get(
+//         "https://www.googleapis.com/oauth2/v3/userinfo",
+//         { headers: { Authorization: `Bearer ${tokenResponse.access_token}` } }
+//       );
+
+//       console.log(userInfo);
+//       alert("Login successful!");
+//     } else {
+//       console.log("Unauthorized user; login failed.");
+//       // Timeout needed to ensure Google login pop-up closes before alert appears
+//       setTimeout(() => {
+//         alert("Unauthorized user; login failed.");
+//       }, 100);
+//     }
+//   },
+//   onError: (errorResponse) => console.log(errorResponse),
+// });
