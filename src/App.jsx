974c0e819de4bc62ff4c2e7b1949acbb2f107045
kgriffo <karsten.griffo@gmail.com>
2025-01-31 11:48:18 -0500

    added useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import "./App.css";
-import { useEffect, useContext, createContext, useState } from "react";
+import { useEffect, useContext, createContext, useState, useRef } from "react";
 import {
   HashRouter as Router,
   Route,
@@ -13,16 +13,16 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   console.log("fired auth provider");
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const token = localStorage.getItem("id_token");
   useEffect(() => {
+    const token = localStorage.getItem("id_token");
     if (token) {
       setIsAuthenticated(true);
     }
-  }, [token]);
+  }, []);
   console.log(isAuthenticated, "checking auth state in App.jsx");
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, token }}>
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
       {children}
     </AuthContext.Provider>
   );
@@ -35,6 +35,7 @@ const UseAuth = () => {
 
 function Auth() {
   const { isAuthenticated, setIsAuthenticated } = UseAuth();
+  const clientRef = useRef(null);
   useEffect(() => {
     const token = localStorage.getItem("id_token");
     if (token) {
@@ -43,20 +44,22 @@ function Auth() {
     console.log(isAuthenticated, "checking auth state in App.jsx");
   }, [isAuthenticated, setIsAuthenticated]);
 
-  const client = window.google.accounts.oauth2.initTokenClient({
-    client_id:
-      "248005865613-j5nc0j164et50emv7g18ap21l64bi07q.apps.googleusercontent.com",
-    redirect_uri: "https://kgriffo.github.io/auth/",
-    response_type: "token id_token",
-    scope: "openid",
-    callback: (tokenResponse) => {
-      console.log("Token Response:", tokenResponse);
-      if (tokenResponse && tokenResponse.id_token) {
-        localStorage.setItem("id_token", tokenResponse.id_token);
-        setIsAuthenticated(true);
-      }
-    },
-  });
+  useEffect(() => {
+    clientRef.current = window.google.accounts.oauth2.initTokenClient({
+      client_id:
+        "248005865613-j5nc0j164et50emv7g18ap21l64bi07q.apps.googleusercontent.com",
+      redirect_uri: "https://kgriffo.github.io/auth/",
+      response_type: "token id_token",
+      scope: "openid",
+      callback: (tokenResponse) => {
+        console.log("Token Response:", tokenResponse);
+        if (tokenResponse && tokenResponse.id_token) {
+          localStorage.setItem("id_token", tokenResponse.id_token);
+          setIsAuthenticated(true);
+        }
+      },
+    });
+  }, [setIsAuthenticated]);
 
   return (
     <>
@@ -81,13 +84,15 @@ function Auth() {
       </Router>
       <div style={{ textAlign: "center", marginTop: "50px" }}>
         <h1>Google OAuth Test</h1>
-        <button onClick={() => client.requestAccessToken()}>Log in</button>
+        <button onClick={() => clientRef.current?.requestAccessToken()}>
+          Log in
+        </button>
       </div>
     </>
   );
 }
 
-export default { Auth, AuthProvider };
+export { Auth };
 
 // const googleLogin = useGoogleLogin({
 //   onSuccess: async (tokenResponse) => {
