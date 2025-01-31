458ed54e7ccd04f112d22593206499d5ec668ca8
kgriffo <karsten.griffo@gmail.com>
2025-01-31 13:16:53 -0500

    forgot to wrap app with Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { AuthProvider } from "./App.jsx";
+import { Router } from "react-router-dom";
 import "./index.css";
 import Auth from "./App.jsx";
 
@@ -9,11 +10,13 @@ const clientId =
   "248005865613-j5nc0j164et50emv7g18ap21l64bi07q.apps.googleusercontent.com";
 
 createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <GoogleOAuthProvider clientId={clientId}>
-      <StrictMode>
-        <Auth />
-      </StrictMode>
-    </GoogleOAuthProvider>
-  </AuthProvider>
+  <Router>
+    <AuthProvider>
+      <GoogleOAuthProvider clientId={clientId}>
+        <StrictMode>
+          <Auth />
+        </StrictMode>
+      </GoogleOAuthProvider>
+    </AuthProvider>
+  </Router>
 );
